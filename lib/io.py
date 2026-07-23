2b8fa3a553b742fa29eb592ea577e1e611434828
Liza Sazonova <sazonova.astro@gmail.com>
2026-07-23 16:19:33 -0400

    fortran indexing

--- lib/io.py
+++ lib/io.py
@@ -74,7 +74,7 @@ def download_cutout(coords, x, y, size, path='/scratch/'):
     vosclient = Client()
     tilename = f'CFIS_LSB.{coords:0.3f}.r'
     if not os.path.exists(f'{path}/cutout_{coords:0.3f}_{x}_{y}.fits'):
-        vosclient.copy(f'vos:cfis/tiles_LSB_DR5/{tilename}.fits[{y-size}:{y+size},{x-size}:{x+size}]', 
+        vosclient.copy(f'vos:cfis/tiles_LSB_DR5/{tilename}.fits[{x-size}:{x+size},{y-size}:{y+size}]', 
                     f'{path}/cutout_{coords:0.3f}_{x}_{y}.fits')
         
 def make_cutout(galaxy, tile, weightmap, segmap, cutout_min=20, r_frac=2):
